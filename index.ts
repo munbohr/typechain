cb2ef83a1ae3255e171bf64e822fe0927032bab1
munbohr <85949748+munbohr@users.noreply.github.com>
2022-03-31 22:06:47 +0900

    update about blockchain

--- index.ts
+++ index.ts
@@ -1,10 +1,23 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index:number;
     public hash:string;
     public previousHash:string;
     public data:string;
     public timestamp:number;
-    constructor(index:number,
+
+    static calculateBlockHash = (
+        index:number,
+        previousHash:string,
+        timestamp:number,
+        data:string) : string => { 
+            return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+    }
+
+    constructor(
+        index:number,
         hash:string,
         previousHash:string,
         data:string,
@@ -17,12 +30,16 @@ class Block {
         }
 }
 
-const genesisBlock: Block= new Block(0, "addad", "", "Hello", 12345)
+//Block.calculateBlockHash()
+
+const genesisBlock: Block= new Block(0, "202203312148", "", "Hello", 123456);
  
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-blockchain.push("stuff");
+const getBlockchain = () : Block[] => blockchain;
 
-console.log(blockchain);
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime()/ 1000);
 
 export {};
