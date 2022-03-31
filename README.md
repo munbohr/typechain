512b7ab20b79f603a141e35af27d1460044b9d9b
munbohr <85949748+munbohr@users.noreply.github.com>
2022-03-31 19:48:04 +0900

    update index.ts % i use README & mv {typechain} ~

---
+++ README.md
@@ -0,0 +1,16 @@
+밑에 변수들은 수업중 필요 없어져서 지운 것들
+console.log("Hello world");
+/////////////////
+const person = {
+nickname : "woojin",
+age : 19,
+gender : "male"
+/////////////////
+interface Human {
+nickname: string;
+age: number;
+gender: string;
+//////////////////
+//variable? === you don't get argument that you will use function
+/////////////////
+/_const sayHi: (name: string, age:number, gender:string) => string _/
--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 //console.log("Hello world");
-const nickname = "woojin",
-    age = 19,
-    gender = "male";    
+class Human {
+    public nickname: string;
+    public age: number; 
+    public gender: string;
+    constructor(nickname: string, age: number, gender: string) {//classr가 호출될 때마다 실행됨
+        this.nickname = nickname; 
+        this.age = age; 
+        this.gender = gender;
+    }
+}
+
+const bohr = new Human("bohr", 19, "male");
+
 
     
-    const  sayHi = (nickname: string, age: number,  gender: string): string =>  { 
-        return `Hello ${nickname}, you are ${age}, you are a ${gender}`;
+    const  sayHi = (person: Human) : string =>  { 
+        return `Hello ${person.nickname}, you are ${person.age}, you are a ${person.gender}!`;
     };
-//variable? === you don't get argument that you will use function
-/*const sayHi: (name:  string, age:number, gender:string) => string */
-console.log(sayHi(nickname, age, gender));
+
+console.log(sayHi(bohr));
 
 export {};
